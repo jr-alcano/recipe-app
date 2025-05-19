20a0d5d7666e85828825b01b39377b028bb980d7
jalcan9 <jalcan9@uic.edu>
2025-05-19 12:18:57 -0500

    attempting fixing railway deploy bug

--- recipe-app/server/index.js
+++ recipe-app/server/index.js
@@ -5,13 +5,19 @@ const path = require("path");
 const PORT = process.env.PORT || 3001;
 
 if (process.env.NODE_ENV === "production") {
-  const clientPath = path.join(__dirname, "..", "client", "build");
+  const clientPath = path.join(__dirname, "client", "build");
   app.use(express.static(clientPath));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(clientPath, "index.html"));
+  app.get("/*", (req, res) => {
+    if (req.accepts("html")) {
+      res.sendFile(path.join(clientPath, "index.html"));
+    } else {
+      res.status(404).json({ error: "Not found" });
+    }
   });
 }
+
+
 
 console.log("Attempting to start server...");
 app.listen(PORT, () => {
