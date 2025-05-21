e9ff8b1cd788f9206165b4ca73ec6f512aab07c9
jalcan9 <jalcan9@uic.edu>
2025-05-21 12:01:52 -0500

    attempting fixing railway deploy bug

--- recipe-app/server/app.js
+++ recipe-app/server/app.js
@@ -38,9 +38,13 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "./client-build")));
 
   app.get("*", (req, res) => {
+    if (req.originalUrl.startsWith("/auth") || req.originalUrl.startsWith("/recipes")) {
+      return res.status(404).json({ error: "Not found" });
+    }
     res.sendFile(path.join(__dirname, "./client-build", "index.html"));
   });
 }
+
 
 
 module.exports = app;
