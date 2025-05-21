d644ce3c5ce499bbe9d4eaadd32b9f2bfb3ece26
jalcan9 <jalcan9@uic.edu>
2025-05-21 15:35:11 -0500

    attempting fixing railway deploy bug

--- recipe-app/server/index.js
+++ recipe-app/server/index.js
@@ -1,7 +1,19 @@
+// server/index.js
+
 const express = require("express");
 const app = require("./app");
 
 const PORT = process.env.PORT || 3001;
+
+// Log any uncaught exceptions or unhandled rejections
+process.on("uncaughtException", (err) => {
+  console.error("🔥 Uncaught Exception:", err);
+  console.error(err.stack);
+});
+
+process.on("unhandledRejection", (reason, promise) => {
+  console.error("🔥 Unhandled Rejection:", reason);
+});
 
 console.log("Attempting to start server...");
 app.listen(PORT, () => {
